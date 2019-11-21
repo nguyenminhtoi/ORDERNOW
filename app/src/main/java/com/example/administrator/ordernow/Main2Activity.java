ce1570054b3b505d6ff0605fbb2f129210270d09
Nguyen Toi <48339647+nguyenminhtoi@users.noreply.github.com>
2019-11-21 20:33:55 +0700

    upload activity admin + order

--- app/src/main/java/com/example/administrator/ordernow/Main2Activity.java
+++ app/src/main/java/com/example/administrator/ordernow/Main2Activity.java
@@ -1,13 +1,47 @@
 package com.example.administrator.ordernow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class Main2Activity extends AppCompatActivity {
-
+    @Bind(R.id.btnAdmin)
+    Button btnAdmin;
+    @Bind(R.id.btnOrder)
+    Button btnOrder;
+    @Bind(R.id.btnLogout)
+    Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
