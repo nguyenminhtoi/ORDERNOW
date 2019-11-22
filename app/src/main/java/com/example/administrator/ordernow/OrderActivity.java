443dd1e19e699ad718501b58f5a40fd15b60d289
Minh Toi <toinm@bw-asia.com.vn>
2019-11-22 11:24:42 +0700

    update list gỉd

--- app/src/main/java/com/example/administrator/ordernow/OrderActivity.java
+++ app/src/main/java/com/example/administrator/ordernow/OrderActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.ordernow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new TableAdapter(this));
     }
 }
